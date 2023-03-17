f0be2b176838fba01526cc8f06e2b7a5162cfe14
umairahmed88 <umairahmed_88@ymail.com>
2023-03-17 07:05:08 +0500

    Cloned a discord app

--- src/components/Channels.js
+++ src/components/Channels.js
@@ -1,23 +1,58 @@
-const Channels = ({ provider, account, dappcord, channels, currentChannel, setCurrentChannel }) => {
+const Channels = ({
+	provider,
+	account,
+	dappcord,
+	channels,
+	currentChannel,
+	setCurrentChannel,
+}) => {
+	const channelHandler = async (channel) => {
+		const hasJoined = await dappcord.hasJoined(channel.id, account);
 
-  return (
-    <div className="channels">
-      <div className="channels__text">
-        <h2>Text Channels</h2>
+		if (hasJoined) {
+			setCurrentChannel(channel);
+		} else {
+			const signer = await provider.getSigner();
+			const transaction = await dappcord
+				.connect(signer)
+				.mint(channel.id, { value: channel.cost });
+			await transaction.wait();
+		}
+	};
 
-      </div>
+	return (
+		<div className='channels'>
+			<div className='channels__text'>
+				<h2>Text Channels</h2>
+				<ul>
+					{channels.map((channel, index) => (
+						<li
+							key={index}
+							onClick={() => channelHandler(channel)}
+							className={
+								currentChannel &&
+								currentChannel.id.toString() === channel.id.toString()
+									? "active"
+									: ""
+							}
+						>
+							{channel.name}
+						</li>
+					))}
+				</ul>
+			</div>
 
-      <div className="channels__voice">
-        <h2>Voice Channels</h2>
+			<div className='channels__voice'>
+				<h2>Voice Channels</h2>
 
-        <ul>
-          <li>Channel 1</li>
-          <li>Channel 2</li>
-          <li>Channel 3</li>
-        </ul>
-      </div>
-    </div>
-  );
-}
+				<ul>
+					<li>Channel 1</li>
+					<li>Channel 2</li>
+					<li>Channel 3</li>
+				</ul>
+			</div>
+		</div>
+	);
+};
 
 export default Channels;
